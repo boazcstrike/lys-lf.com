44982d26632f7bbb8e4e50b43f853198eac3e286
Boaz Sze <boaz.sze@gmail.com>
2026-01-15 03:12:42 +0800

    added upgrades, google analytics

--- lys-lf-react-19/app/layout.tsx
+++ lys-lf-react-19/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import ErrorBoundary from "@/app/components/error-boundary";
 import "@/app/styles/globals.css";
 import "@/app/styles/index.scss";
@@ -153,6 +154,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
           {children}
         </ErrorBoundary>
       </body>
+      {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
+      )}
     </html>
   );
 }
